dba48218e3cd4e394a05bfe7a4c89e06851be3ea
dbsdndcks <ywcsuwon@naver.com>
2025-12-15 14:34:28 +0900

    chore: 프로젝트 초기 설정

---
+++ backend/src/main/java/com/healthy_plate/HealthyPlateApplication.java
@@ -0,0 +1,13 @@
+package com.healthy_plate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyPlateApplication.class, args);
+	}
+
+}
