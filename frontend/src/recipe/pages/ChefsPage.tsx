1be024f5d01ee164d7214b5a0d5e67ad40987973
kimbyeongnyeon <qud5252@naver.com>
2025-12-18 01:31:37 +0900

    feat(#18): 셰프 레시피 페이지 구현

--- frontend/src/recipe/pages/ChefsPage.tsx
+++ frontend/src/recipe/pages/ChefsPage.tsx
@@ -1,17 +1,19 @@
 import { useResponsive } from "../../share/hooks/useResonsive";
+import { RecipeList } from "../components/RecipeList";
 // import { RecipeList } from "../components/RecipeList";
-import { useTrendRecipe } from "../hooks/useTrendRecipe";
+import { useChefsRecipe } from "../hooks/useChefsRecipe";
 
 export const ChefsPage = () => {
-  const { isLoading } = useTrendRecipe();
+  const { isLoading, recipes } = useChefsRecipe();
   const { gridCols } = useResponsive();
+
   if (isLoading) return <div>로딩중...</div>;
 
   return (
     <div
       className={`grid gap-4 ${gridCols === 4 ? "grid-cols-4" : "grid-cols-2"}`}
     >
-      hi
+      <RecipeList recipes={recipes} />
     </div>
   );
 };
