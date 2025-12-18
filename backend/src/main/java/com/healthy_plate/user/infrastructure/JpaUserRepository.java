90432609d721192bde23c4ba10633fd5d25dfaf5
dbsdndcks <ywcsuwon@naver.com>
2025-12-18 16:09:58 +0900

    feat(#19): User 리포지토리 구현

--- backend/src/main/java/com/healthy_plate/user/infrastructure/JpaUserRepository.java
+++ backend/src/main/java/com/healthy_plate/user/infrastructure/JpaUserRepository.java
@@ -1,10 +1,13 @@
 package com.healthy_plate.user.infrastructure;
 
+import com.healthy_plate.auth.domain.model.OAuth2Provider;
 import com.healthy_plate.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaUserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByProviderAndProviderId(OAuth2Provider provider, String providerId);
 }
