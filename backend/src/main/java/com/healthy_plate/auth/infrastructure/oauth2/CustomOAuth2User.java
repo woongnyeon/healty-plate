45e5f056815e5ae76160c9d7ba7bac7002d77af1
dbsdndcks <ywcsuwon@naver.com>
2025-12-18 16:40:57 +0900

    feat(#21): OAuth 로그인 서비스 구현

---
+++ backend/src/main/java/com/healthy_plate/auth/infrastructure/oauth2/CustomOAuth2User.java
@@ -0,0 +1,47 @@
+package com.healthy_plate.auth.infrastructure.oauth2;
+
+import com.healthy_plate.user.domain.UserRole;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User {
+
+    private final Long userId;
+    private final String email;
+    private final UserRole role;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(
+        final Long userId,
+        final String email,
+        final UserRole role,
+        final Map<String, Object> attributes
+    ) {
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+        this.attributes = attributes;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getName() {
+        return email;
+    }
+}
