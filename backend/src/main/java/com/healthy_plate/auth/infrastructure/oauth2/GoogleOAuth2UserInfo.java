0214e87fdedce83e29c5e43c2338c1ab74ce5c3c
dbsdndcks <ywcsuwon@naver.com>
2025-12-18 16:38:35 +0900

    feat(#21): 구글 OAuth2User 구현
    - 인터페이스로 추상화
    - 네이버, 카카오 추가 예정

---
+++ backend/src/main/java/com/healthy_plate/auth/infrastructure/oauth2/GoogleOAuth2UserInfo.java
@@ -0,0 +1,34 @@
+package com.healthy_plate.auth.infrastructure.oauth2;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
+
+    private static final String PROVIDER = "google";
+
+    private final Map<String, Object> attributes;
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProvider() {
+        return PROVIDER;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+}
