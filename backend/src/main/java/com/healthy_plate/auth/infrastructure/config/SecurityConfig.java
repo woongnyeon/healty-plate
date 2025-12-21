ce039aed16dcc540631a318f6f197b806e6ff344
dbsdndcks <ywcsuwon@naver.com>
2025-12-21 15:17:56 +0900

    feat: cors 설정 추가

--- backend/src/main/java/com/healthy_plate/auth/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/healthy_plate/auth/infrastructure/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configure(http))
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
