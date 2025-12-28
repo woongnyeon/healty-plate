0df63c0a76ad675ecc3130c783ceb7b2164c948e
kimbyeongnyeon <qud5252@naver.com>
2025-12-29 03:46:13 +0900

    feat(#45): 작성 페이지 왼쪽 부분에는 레시피 작성 오른쪽 부분에는 미리보기

--- frontend/src/edit/pages/EditPage.tsx
+++ frontend/src/edit/pages/EditPage.tsx
@@ -1,3 +1,19 @@
+import { EditSection } from "../components/Edit/EditSection";
+import { PreviewSection } from "../components/Preview/PreviewSection";
+
 export const EditPage = () => {
-  return <div className="mt-16">h1</div>;
+  return (
+    <div className="h-screen overflow-hidden bg-white pt-16">
+      <div className="mx-auto flex h-full max-w-full flex-col px-4 md:px-6 lg:px-8">
+        <div className="pt-6" />
+
+        <div className="flex-1 overflow-y-auto pb-10">
+          <div className="grid grid-cols-1 lg:grid-cols-[1.6fr_1fr] gap-10">
+            <EditSection />
+            <PreviewSection />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
