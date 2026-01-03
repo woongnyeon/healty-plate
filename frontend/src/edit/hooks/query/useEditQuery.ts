a7d5d475f49399a6069460015e203e1a430a9193
kimbyeongnyeon <qud5252@naver.com>
2026-01-04 01:34:58 +0900

    feat(#45): 커스텀 훅 분리 tanstack-query 를 담당할 훅

---
+++ frontend/src/edit/hooks/query/useEditQuery.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Ingredient } from "../../types/Ingredient";
+import { useEditApi } from "../api/useEditApi";
+
+export const useEditQuery = () => {
+  const { fetchAllIngredients } = useEditApi();
+  const ingredientQuery = () => {
+    return useQuery<Ingredient[]>({
+      queryKey: ["ingredients", "all"],
+      queryFn: fetchAllIngredients,
+      staleTime: 1000 * 60 * 10,
+    });
+  };
+
+  return {
+    ingredientQuery,
+  };
+};
