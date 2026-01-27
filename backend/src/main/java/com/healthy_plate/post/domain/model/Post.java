03601e46c59d3d4be8fe49c2f5358e5f8b33d87d
DEV_WOONG <ywcsuwon@gmail.com>
2026-01-27 18:25:48 +0900

    feat(#50): Post 도메인 1차 구현

--- backend/src/main/java/com/healthy_plate/post/domain/model/Post.java
+++ backend/src/main/java/com/healthy_plate/post/domain/model/Post.java
@@ -1,12 +1,16 @@
 package com.healthy_plate.post.domain.model;
 
 import com.healthy_plate.shared.domain.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +28,9 @@ public class Post extends BaseEntity {
 
     @Column(name = "writer_id")
     private Long writerId;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostImage> postImages = new ArrayList<>();
 
     @Column(length = 200, name = "title")
     private String title;
@@ -48,4 +55,9 @@ public class Post extends BaseEntity {
 
     @Column(name = "is_deleted")
     private boolean isDeleted;
+
+    public void addImage(final PostImage image) {
+        postImages.add(image);
+        image.assignPost(this);
+    }
 }
